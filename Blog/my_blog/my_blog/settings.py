03c50af07c8e9842278c5d7868ddf4f930dc466b
MEKSEM Kamal <meksem.k@gmail.com>
2021-02-27 20:33:38 +0100

    commit second

--- Blog/my_blog/my_blog/settings.py
+++ Blog/my_blog/my_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-
+SECRET_KEY = '0&7r8nbx29cj#n7y3!fo6_f)%2kluw%4780^dipc-87=n)^58c'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
