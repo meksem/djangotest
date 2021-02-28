d77ec0df6aaceb344643f63f9a5fd2888a9b985e
MEKSEM Kamal <meksem.k@gmail.com>
2021-02-28 20:07:00 +0100

    commit all

--- Blog/my_blog/blog/views.py
+++ Blog/my_blog/blog/views.py
@@ -3,5 +3,6 @@ from django.shortcuts import render
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
 
-
+def add(request):
+    return render(request,'result.htm')
 # Create your views here.
